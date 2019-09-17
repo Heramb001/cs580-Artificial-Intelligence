d578418195d8d42afc934f5a04422aca8b4c8723
Heramb Pendyala <hpend001@odu.edu>
2019-09-17 10:46:25 -0400

    adding initial code files

---
+++ 01/sherlock.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 16 21:03:51 2019
+
+@author: HERAMB
+
+24 puzzle problem
+
+Initial State
+--------------------------
+| 9  | 24 |	3  | 5  | 17 |
+--------------------------
+| 6	 |    | 13 | 19 | 10 |
+--------------------------
+| 11 | 21 |	12 | 1  | 20 |
+--------------------------
+| 16 | 4  | 14 | 12 | 15 |
+--------------------------
+| 8  | 18 |	23 | 2  |  7 |
+--------------------------
+
+---- 0 is a blank tile which can be moved
+
+
+Goal State
+--------------------------
+| 1  | 2  | 3  | 4  | 5  | 
+--------------------------
+| 6  | 7  | 8  | 9  | 10 | 
+--------------------------
+| 11 | 12 | 13 | 14 | 15 | 
+--------------------------
+| 16 | 17 | 18 | 19 | 20 | 
+--------------------------
+| 21 | 22 | 23 | 24 |  0 | 
+--------------------------
+
+"""
+#import libraries
+from math import sqrt
+from collections import deque
+from state import State
+import time
+
+
+#--- Specify a goal state which will be tested to stop the program.
+goalState = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,0]
+goalNode = State
+initialState = []   #--- Initial State to be empty List
+puzzleLen = 0       #--- Length of the puzzle (Eg - for 24 puzzle problem, len = 25)  
+puzzleSide = 0      #--- Length of puzzleSide (Eg - for 24 puzzle problem, side = 5)
+
+nodesExpanded = 0   #--- no of nodes expanded, will be used in the end, initial = 0
+maxDepthReached = 0 #--- Length of the depth reached
+maxFringeSize = 0   #--- Maximum Size of Fringe, will be used in the end, initial = 0
+
+moves = []          #--- this keeps track of all the moves that are required to reach the goal state
+
+
+"""
+Function - bfs() (Breadth First Search)
+    - works as a Breadth First Search algorithm.
+"""
+def bfs(startState):
+
+    global goalNode, maxFringeSize, maxDepthReached
+    
+    visited, queue = set(), deque([State(startState, None, None, 0, 0, 0)])
+    while queue:
+        node = queue.popleft()
+        
+        visited.add(node.map)
+        
+        #--- Goal Test
+        if node.state == goalState:
+            goalNode = node
+            return True, queue
+        
+        #--- If not a goal state then expand the node
+        childNodes = expand(node)
+        
+        for child in childNodes:            #--- Traverse every child in the depth
+            if child.map not in visited:    #--- Check if visited or not
+                queue.append(child)         #--- if not visited append as a child
+                visited.add(child.map)      #--- add it to the visited nodes set
+                
+                if child.depth > maxDepthReached:
+                    maxDepthReached += 1
+        
+        if len(queue) > maxFringeSize:
+            maxFringeSize = len(queue)
+    
+    #--- if search is complete and goal state not reached then return goal not found
+    return False, None       
+    
+"""
+Function - dfs() (Breadth First Search)
+    - works as a Depth First Search algorithm.
+
+"""
+def dfs(startState):
+    
+    global goalNode, maxFringeSize, maxDepthReached
+    
+    visited, stack = set(), list([State(startState, None, None, 0, 0, 0)])
+
+    while stack:
+
+        node = stack.pop()
+
+        visited.add(node.map)
+
+        if node.state == goalState:
+            goalNode = node
+            return True, stack
+
+        neighbors = reversed(expand(node))
+
+        for neighbor in neighbors:
+            if neighbor.map not in visited:
+                stack.append(neighbor)
+                visited.add(neighbor.map)
+
+                if neighbor.depth > maxDepthReached:
+                    maxDepthReached += 1
+
+        if len(stack) > maxFringeSize:
+            maxFringeSize = len(stack)
+    #--- if search is complete and goal state not reached then return goal not found
+    return False, None
+
+    
+"""
+Function - expand()
+    - expands the node and creates valid child nodes
+    - returns all valid child nodes of the current node 
+"""
+def expand(node):
+    global nodesExpanded
+    nodesExpanded += 1
+    childNodes = []
+    
+    #Append all the child to childNode for a valid move
+    childNodes.append(State(validMove(node.state,'RIGHT'),node,'RIGHT',node.depth + 1, node.cost + 1, 0)) #--- Top
+    childNodes.append(State(validMove(node.state,'DOWN'),node,'DOWN',node.depth + 1, node.cost + 1, 0)) #--- Down
+    childNodes.append(State(validMove(node.state,'LEFT'),node,'LEFT',node.depth + 1, node.cost + 1, 0)) #--- Left
+    childNodes.append(State(validMove(node.state,'UP'),node,'UP',node.depth + 1, node.cost + 1, 0)) #--- Right
+    
+    nodes = [child for child in childNodes if child.state]
+    return nodes
+
+"""
+Function validMove()
+    - validates the next move as valid or invalid
+    - returns valid state if move is valid otherwise returns None
+"""
+def validMove(state, position):
+    newState = state[:]
+    index = newState.index(0) #--- get the position of blank tile
+    if position == 'UP':  # Up
+
+        if index not in range(0, puzzleSide): #--- Valid iff not present in top row
+            #--- Swap the empty tile with top element
+            temp = newState[index - puzzleSide]
+            newState[index - puzzleSide] = newState[index]
+            newState[index] = temp
+
+            return newState
+        else:
+            return None
+
+    if position == 'DOWN':  # Down
+        #--- Swap the empty tile with bottom element
+        if index not in range(puzzleLen - puzzleSide, puzzleLen):
+
+            temp = newState[index + puzzleSide]
+            newState[index + puzzleSide] = newState[index]
+            newState[index] = temp
+
+            return newState
+        else:
+            return None
+
+    if position == 'LEFT':  # Left
+
+        if index not in range(0, puzzleLen, puzzleSide):
+
+            temp = newState[index - 1]
+            newState[index - 1] = newState[index]
+            newState[index] = temp
+
+            return newState
+        else:
+            return None
+
+    if position == 'RIGHT':  # Right
+
+        if index not in range(puzzleSide - 1, puzzleLen, puzzleSide):
+
+            temp = newState[index + 1]
+            newState[index + 1] = newState[index]
+            newState[index] = temp
+
+            return newState
+        else:
+            return None
+"""
+Function - get(dataList) 
+    - Reads input from user and updates puzzle configuration
+"""
+def get(dataList):
+    global puzzleLen, puzzleSide
+    
+    data = dataList.split(',') 
+    for element in data:
+        initialState.append(int(element))
+    puzzleLen = len(initialState)                   #--- get the length f puzzle
+    puzzleSide = int(sqrt(puzzleLen))       #--- calculate square root in order to get the length of puzzle size
+
+"""
+Function - backtrack()
+"""
+def backtrack():
+    currentNode = goalNode
+    while initialState != currentNode.state : #--- terminating condition when we reach top node from bottom 
+        moves.insert(0, currentNode.move)
+        currentNode = currentNode.parent
+    return moves
+
+"""
+Function - output(fringe, time)
+    - creates an output file with all the required elements
+"""
+def output(fringe, time):
+    if fringe:
+        
+        global moves
+        
+        moves = backtrack() #--- get all the moves performed to reach the goal state
+        file = open('testcase_dfs.txt', 'w')
+        file.write("\npath_to_goal: " + str(moves))
+        file.write("\ncost_of_path: " + str(len(moves)))
+        file.write("\nnodes_expanded: " + str(nodesExpanded))
+        file.write("\nfringe_size: " + str(len(fringe)))
+        file.write("\nmax_fringe_size: " + str(maxFringeSize))
+        file.write("\nsearch_depth: " + str(goalNode.depth))
+        file.write("\nmax_search_depth: " + str(maxDepthReached))
+        file.write("\nrunning_time: " + format(time, '.8f'))
+        file.close()
+    else :
+        file = open('testcase_unsolvable.txt', 'w')
+        file.write("<-- # UNSOLVABLE # -->")
+        file.write("\nnodes_expanded: " + str(nodesExpanded))
+        file.write("\nmax_fringe_size: " + str(maxFringeSize))
+        file.write("\nmax_search_depth: " + str(maxDepthReached))
+        file.write("\nrunning_time: " + format(time, '.8f'))
+        file.close()
+
+"""
+Function - main() 
+    - Executed everytime the python file starts.
+"""
+def main():
+    algorithm = input('--> Please enter the algorithm : ')
+    
+    data = input('--> Enter puzzle elements : ')
+    
+    get(data)
+    
+    function = function_map[algorithm]
+    
+    start = time.time()
+
+    search, fringe = function(initialState)
+
+    stop = time.time()
+    
+    if search : 
+        output(fringe, stop-start)
+    else : 
+        print('Validated all the possible states but goal state not found')
+        print('<-- # UNSOLVABLE # -->')
+        output(fringe, stop-start)
+
+function_map = {
+    'bfs': bfs,
+    'dfs': dfs
+    }
+
+if __name__ == '__main__':
+    main()
---
+++ 01/state.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 15 11:37:52 2019
+
+@author: HERAMB
+
+state  - Location of all the tiles, including blank one
+parent - parent of the current state
+move   - from which move did we obtain the current state
+depth  - depth of current state
+cost   - cost of current state
+key    -  
+
+
+"""
+
+class State:
+
+    def __init__(self, state, parent, move, depth, cost, key):
+
+        self.state = state
+
+        self.parent = parent
+
+        self.move = move
+
+        self.depth = depth
+
+        self.cost = cost
+
+        self.key = key
+
+        if self.state:
+            self.map = ''.join(str(e) for e in self.state)
+
+    def __eq__(self, other):
+        return self.map == other.map
+
+    def __lt__(self, other):
+        return self.map < other.map
